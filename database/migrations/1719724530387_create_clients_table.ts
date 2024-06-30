00aae71cdd19dbc0e48ecf3ef611fb601b8d42f8
Julio Matias <juliocesark2@hotmail.com>
2024-06-30 02:24:15 -0300

    feat: implement clients migration

---
+++ database/migrations/1719724530387_create_clients_table.ts
@@ -0,0 +1,18 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'clients'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').notNullable().primary()
+
+      table.string('name').notNullable()
+      table.string('tax_id').notNullable().unique()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
