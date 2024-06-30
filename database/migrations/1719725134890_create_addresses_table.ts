1e3995c9fe2e0c7a14ee3251d26a1a440f418bc6
Julio Matias <juliocesark2@hotmail.com>
2024-06-30 02:39:32 -0300

    feat: implement addresses migration

---
+++ database/migrations/1719725134890_create_addresses_table.ts
@@ -0,0 +1,29 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'addresses'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').notNullable().primary()
+
+      table.string('street', 150).notNullable()
+      table.string('number', 8).notNullable()
+      table.string('city', 60).notNullable()
+      table.string('state', 60).notNullable()
+      table.string('country', 60).notNullable()
+      table.string('zip', 15).notNullable()
+      table
+        .integer('client_id', 10)
+        .unsigned()
+        .references('id')
+        .inTable('clients')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
