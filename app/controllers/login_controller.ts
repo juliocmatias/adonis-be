eb587e871fdd0b3d6ee61cc952219e06da061072
Julio Matias <juliocesark2@hotmail.com>
2024-07-01 21:34:58 -0300

    feat: implement login registration

---
+++ app/controllers/login_controller.ts
@@ -0,0 +1,13 @@
+import LoginService from '#services/login_service'
+import type { HttpContext } from '@adonisjs/core/http'
+import mapStatusHTTP from '../utils/map_status_http.js'
+
+export default class LoginController {
+  constructor(private loginService = new LoginService()) {}
+
+  async store({ request, response }: HttpContext) {
+    const { email, password } = request.all()
+    const { status, data } = await this.loginService.store(email, password)
+    return response.status(mapStatusHTTP(status)).json(data)
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -9,6 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 import '../routes/users_routes.js'
+import '../routes/login_routes.js'
 
 router.get('/', async () => {
   return {
