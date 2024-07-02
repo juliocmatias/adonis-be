2f4ddc6b5bfde3c7f22797d0f3776d4b632e2b12
Julio Matias <juliocesark2@hotmail.com>
2024-07-02 00:13:45 -0300

    refactor: added user_auth model

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,15 +1,6 @@
-import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
-import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
-import { compose } from '@adonisjs/core/helpers'
-import hash from '@adonisjs/core/services/hash'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 
-const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
-  uids: ['email'],
-  passwordColumnName: 'password',
-})
-
-export default class User extends compose(BaseModel, AuthFinder) {
+export default class User extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
@@ -18,6 +9,4 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column()
   declare password: string
-
-  static accessTokens = DbAccessTokensProvider.forModel(User)
 }
