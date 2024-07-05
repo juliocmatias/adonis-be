acacd6f02ff6d9e54e120701fbc0ade57c7c4fa8
Julio Matias <juliocesark2@hotmail.com>
2024-07-04 23:13:16 -0300

    feat: implement seeder address

---
+++ database/seeders/address_seeder.ts
@@ -0,0 +1,27 @@
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Address from '#models/address'
+
+export default class extends BaseSeeder {
+  async run() {
+    await Address.createMany([
+      {
+        street: 'Winterfell Street',
+        number: '1',
+        city: 'Winterfell',
+        state: 'The North',
+        country: 'Westeros',
+        zip: '12345-678',
+        clientId: 1,
+      },
+      {
+        street: 'Dragonstone Street',
+        number: '2',
+        city: 'Dragonstone',
+        state: 'Crownlands',
+        country: 'Westeros',
+        zip: '98765-432',
+        clientId: 2,
+      },
+    ])
+  }
+}
