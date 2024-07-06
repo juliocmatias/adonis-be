a4c0293bccd78c0d2a92e7bee7dabf473191aec8
Julio Matias <juliocesark2@hotmail.com>
2024-07-06 05:06:04 -0300

    chore: added swagger

--- start/routes.ts
+++ start/routes.ts
@@ -8,6 +8,8 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import AutoSwagger from 'adonis-autoswagger'
+import swagger from '#config/swagger'
 import '../routes/users_routes.js'
 import '../routes/login_routes.js'
 import '../routes/client_routes.js'
@@ -18,4 +20,16 @@ router.get('/', async () => {
   return {
     ok: true,
   }
+})
+
+// returns swagger in YAML
+router.get('/swagger', async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger)
+})
+
+// Renders Swagger-UI and passes YAML-output of /swagger
+router.get('/docs', async () => {
+  return AutoSwagger.default.ui('/swagger', swagger)
+  // return AutoSwagger.default.scalar("/swagger", swagger); to use Scalar instead
+  // return AutoSwagger.default.rapidoc("/swagger", swagger); to use RapiDoc instead
 })
